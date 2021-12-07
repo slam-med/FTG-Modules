0482065401553061335cb29024d0e63cfbb76218
wich <90055329+wich1@users.noreply.github.com>
2021-12-07 21:42:42 +0200

    Create korabl.py

---
+++ korabl.py
@@ -0,0 +1,27 @@
+from .. import loader
+from asyncio import sleep
+
+@loader.tds
+class HentaiMod(loader.Module):
+	strings = {'name':'Hentai Module'}
+	@loader.owner
+	async def nhcmd(self,message_id):
+		await message_id.edit("плыли мы по морю")
+		await sleep(3,5)
+    await message_id.edit("ветер май штурвал")
+		await sleep(3,5)
+    await message_id.edit("капитан залупа с корабля сбежал,")
+		await sleep(3,5)
+    await message_id.edit("я стоял на вахте и держал весло")
+		await sleep(3,5)
+    await message_id.edit("чем то уебало и меня снесло,")
+		await sleep(3,5)
+    await message_id.edit("я плыву по морю и весь хуй промок,")
+		await sleep(3,5)
+    await message_id.edit("пока ебался в жопу ")
+		await sleep(3,5)
+    await message_id.edit("приплыли на какой то островок ")
+		await sleep(3,5)
+    await message_id.edit("на острове нету не души ")
+		await sleep(3,5)
+    await message_id.edit("хоть садись на камни и яйцо чеши")
