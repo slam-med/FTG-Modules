dba541a410654e2f10c4467df784a5a37361edb5
wich <90055329+wich1@users.noreply.github.com>
2021-12-07 21:45:38 +0200

    Update korabl.py

--- korabl.py
+++ korabl.py
@@ -2,26 +2,28 @@ from .. import loader
 from asyncio import sleep
 
 @loader.tds
-class HentaiMod(loader.Module):
-	strings = {'name':'Hentai Module'}
+class KorablMod(loader.Module):
+	"""Мем (Капитан Залупа С Корабля Сбежал)"""
+	strings = {'name': 'Korabl'}
 	@loader.owner
-	async def nhcmd(self,message_id):
-		await message_id.edit("плыли мы по морю")
-		await sleep(3,5)
-    await message_id.edit("ветер май штурвал")
-		await sleep(3,5)
-    await message_id.edit("капитан залупа с корабля сбежал,")
-		await sleep(3,5)
-    await message_id.edit("я стоял на вахте и держал весло")
-		await sleep(3,5)
-    await message_id.edit("чем то уебало и меня снесло,")
-		await sleep(3,5)
-    await message_id.edit("я плыву по морю и весь хуй промок,")
-		await sleep(3,5)
-    await message_id.edit("пока ебался в жопу ")
-		await sleep(3,5)
-    await message_id.edit("приплыли на какой то островок ")
-		await sleep(3,5)
-    await message_id.edit("на острове нету не души ")
-		await sleep(3,5)
-    await message_id.edit("хоть садись на камни и яйцо чеши")
+	async def korablcmd(self,message_id):
+	"""start"""
+	await message_id.edit("плыли мы по морю")
+	await sleep(3,5)
+    	await message_id.edit("ветер май штурвал")
+	await sleep(3,5)
+    	await message_id.edit("капитан залупа с корабля сбежал,")
+	await sleep(3,5)
+    	await message_id.edit("я стоял на вахте и держал весло")
+	await sleep(3,5)
+    	await message_id.edit("чем то уебало и меня снесло,")
+	await sleep(3,5)
+   	await message_id.edit("я плыву по морю и весь хуй промок,")
+	await sleep(3,5)
+   	await message_id.edit("пока ебался в жопу ")
+	await sleep(3,5)
+   	await message_id.edit("приплыли на какой то островок ")
+	await sleep(3,5)
+    	await message_id.edit("на острове нету не души ")
+	await sleep(3,5)
+    	await message_id.edit("хоть садись на камни и яйцо чеши")
