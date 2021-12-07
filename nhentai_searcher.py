c6e907c1760ffb5af98ba9531f08efb76160715d
seesonrise <89902479+seesonrise@users.noreply.github.com>
2021-12-08 00:08:26 +0300

    Add files via upload

---
+++ nhentai_searcher.py
@@ -0,0 +1,26 @@
+#Create users seesonrise
+#My github github.com/seesonrise
+#Github Wich github.com/wich1
+#This module has a Copyright please do not copy this code
+
+
+
+from .. import loader
+from asyncio import sleep
+import random
+@loader.tds
+class HentaiMod(loader.Module):
+	strings = {'name':'Hentai Module'}
+	@loader.owner
+	async def nhcmd(self,message_id):
+		await message_id.edit("Поиск хентыча")
+		await sleep(0,5)
+		for _ in range(3):
+			for search in ['Поиск.','Поиск..','Поиск...']:
+				await message_id.edit(search)
+				await sleep(0,5)
+		await sleep(1)
+		await message_id.edit("Ваш хентай")
+		await sleep(1)
+		x = random.randint(1,999999)
+		await message_id.edit("nhentai.net/g/"+ str(x))
