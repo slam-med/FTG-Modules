9cc6664cc74b45190fc7a8531602abf51dba05cd
wich <90055329+wich1@users.noreply.github.com>
2021-12-07 20:52:59 +0200

    Add files via upload

---
+++ gaz.py
@@ -0,0 +1,44 @@
+#created by @wich
+# v1.0
+
+from time import sleep
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.gaz(?: |$)(.*)')
+async def typewriter(typew):
+    await typew.edit(" Китайская газета -Хуй вам, хуй нам- ")
+    sleep(4)
+    await typew.edit(" сообщает - что Француский король ")
+    sleep(4)
+    await typew.edit(" Трипер второй выдаёт свою дочь, Ля Курву Дэ Бля Ве ")
+    sleep(4)
+    await typew.edit(" замуж за французского подданого Дон-Дон Штопаный Гандон. ")
+    sleep(4)
+    await typew.edit(" На пир были приглашены ")
+    sleep(4)
+    await typew.edit(" с Румынской стороны Залупыська, ")
+    sleep(4)
+    await typew.edit(" с русской Иван-Пиздарван, ")
+    sleep(4)
+    await typew.edit(" с грузинской Целкаламидзе, ")
+    sleep(4)
+    await typew.edit(" с китайской Сунь Хуй в Чай? ")
+    sleep(4)
+    await typew.edit(" Вынь Сухим и дай подержать другим. ")
+    sleep(4)
+    await typew.edit(" На обед были поданы - супы горячие, хуи стаоячие. ")
+    sleep(4)
+    await typew.edit(" После обеда был просмотр фильма ")
+    sleep(4)
+    await typew.edit(" -Не одна я в поле кувыркалась, не одной мне ветер в жопу дул-. ")
+    sleep(4)
+    await typew.edit(" После этого был показ спектакля ")
+    sleep(4)
+    await typew.edit(" -Как батька Махно выставил жопу в окно-. ")
+    sleep(4)
+    await typew.edit(" И после этого была пропета украиньска народна письня ")
+    sleep(4)
+    await typew.edit(" -Ох, да залупылась на хую шкурынка- ")
+    sleep(4)
+    await typew.edit(" Сегоднешняя газета зкончилась ")#пасхалочка
