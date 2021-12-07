b40817c095cf49ea655664cde054ac4036200ef9
wich <90055329+wich1@users.noreply.github.com>
2021-12-07 23:33:59 +0200

    Update nhentai_searcher.py

--- nhentai_searcher.py
+++ nhentai_searcher.py
@@ -1,18 +1,20 @@
-#Create users seesonrise
+#Create users seesonrise for @wich1
 #My github github.com/seesonrise
-#Github Wich github.com/wich1
+#Github @wich1 github.com/wich1
 #This module has a Copyright please do not copy this code
-
 
 
 from .. import loader
 from asyncio import sleep
 import random
+
 @loader.tds
 class HentaiMod(loader.Module):
-	strings = {'name':'Hentai Module'}
+	"""Дает рандомную ссылку на nhetai"""
+	strings = {'name': 'Hentai Module'}
 	@loader.owner
 	async def nhcmd(self,message_id):
+	"""Начать поиск"""
 		await message_id.edit("Поиск хентыча")
 		await sleep(0,5)
 		for _ in range(3):
