7fb1723b46c4f0a7aadd3d5dd5b87add8b87fde6
wich <90055329+wich1@users.noreply.github.com>
2021-12-07 21:19:21 +0200

    Update korabl.py

--- korabl.py
+++ korabl.py
@@ -6,28 +6,29 @@ from asyncio import sleep
 
 @loader.tds
 class KorablMod(loader.Module):
-    """Мем (Капитан Залупа С Корабля Сбежал)"""
-	strings = {'name':'Korabl'}
-	@loader.owner
-	async def korablcmd(self, message_id):
-		await message_id.edit("плыли мы по морю")
-            await sleep(5)
-        await message_id.edit('ветер май штурвал')
-            await sleep(5)
-        await message_id.edit('капитан залупа с корабля сбежал,')
-            await sleep(5)
-        await message_id.edit('я стоял на вахте и держал весло')
-            await sleep(5)
-        await message_id.edit('чем то уебало и меня снесло,')
-            await sleep(5)
-        await message_id.edit('я плыву по морю')
-            await sleep(5)
-        await message_id.edit('и весь хуй промок,')
-            await sleep(5)
-        await message_id.edit('пока ебался в жопу')
-            await sleep(5)
-        await message_id.edit('приплыли на какой-то островок')
-            await sleep(5)
-        await message_id.edit('на острове нету не души')
-            await sleep(5)
-        await message_id.edit('хоть садись на камни и яйцо чеши')
+	"""Мем (Капитан Залупа С Корабля Сбежал)"""
+	strings = {'name': 'Korabl'}
+		@loader.owner
+		async def korablcmd(self, message_id):
+			"""start"""
+			await message_id.edit("плыли мы по морю")
+            			await sleep(5)
+        		await message_id.edit('ветер май штурвал')
+            			await sleep(5)
+        		await message_id.edit('капитан залупа с корабля сбежал,')
+            			await sleep(5)
+        		await message_id.edit('я стоял на вахте и держал весло')
+            			await sleep(5)
+        		await message_id.edit('чем то уебало и меня снесло,')
+            			await sleep(5)
+        		await message_id.edit('я плыву по морю')
+            			await sleep(5)
+        		await message_id.edit('и весь хуй промок,')
+            			await sleep(5)
+        		await message_id.edit('пока ебался в жопу')
+            			await sleep(5)
+        		await message_id.edit('приплыли на какой-то островок')
+            			await sleep(5)
+        		await message_id.edit('на острове нету не души')
+            			await sleep(5)
+        		await message_id.edit('хоть садись на камни и яйцо чеши')
