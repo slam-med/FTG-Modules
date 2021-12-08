14893fb9f726e914df1f58ee7fa6d992ef278511
seesonrise <89902479+seesonrise@users.noreply.github.com>
2021-12-08 12:48:01 +0300

    update Premium.py

--- Premium.py
+++ Premium.py
@@ -2,12 +2,13 @@ from .. import loader, utils
 
 @loader.tds
 class PremMod(loader.Module):
-  """License Checker"""
+  
   strings ={'name':'Test Premium'}
+  
   @loader.owner
   async def premcmd (self,message):
     """ Проверка лицензии"""
     if message == gopabobra:
       await utils.answer(message," Лицензия робит")
-    elif
+    else
       await utils.answer(message, "Ярик всё хуйня")
