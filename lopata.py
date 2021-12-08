cdc3b20486c17a24e6b07813957417bf92e91236
seesonrise <89902479+seesonrise@users.noreply.github.com>
2021-12-08 16:03:01 +0200

    Update lopata.py

--- lopata.py
+++ lopata.py
@@ -5,7 +5,7 @@ class LopataMod(loader.Module):
 	"""На лопату в лицо"""
 	async def lopatacmd (self,message):
 		"""Лопату в лицо"""
-		strings = {'name'='Обычная лопата'}
+		strings = {'name':'Обычная лопата'}
 		await utils.answer(message,"лопату заказывали")
 		await sleep(1)
 		await utils.answer(message,"Нет?")
