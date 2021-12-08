2c22f63477762044349afb485ad3eddd19f0680e
wich <90055329+wich1@users.noreply.github.com>
2021-12-08 11:38:20 +0200

    update gazeta.py

--- gazeta.py
+++ gazeta.py
@@ -1,6 +1,6 @@
 #created by @wich1
 
-from .. import loader
+from .. import loader, utils
 from asyncio import sleep
 
 
@@ -10,38 +10,38 @@ class GazetaMod(loader.Module):
 	strings = {'name': 'Gazeta'}
 	@loader.owner
 	async def gazetacmd(self, message_id):
-		await message_id.edit(" Китайская газета -Хуй вам, хуй нам- ")
+		await utils.answer(message, " Китайская газета -Хуй вам, хуй нам- ")
 		await asyncio.sleep(4)
-		await message_id.edit(" сообщает - что Француский король ")
+		await utils.answer(message, " сообщает - что Француский король ")
 		await asyncio.sleep(4)
-		await message_id.edit(" Трипер второй выдаёт свою дочь, Ля Курву Дэ Бля Ве ")
+		await utils.answer(message, " Трипер второй выдаёт свою дочь, Ля Курву Дэ Бля Ве ")
 		await asyncio.sleep(4)
-		await message_id.edit(" замуж за французского подданого Дон-Дон Штопаный Гандон. ")
+		await utils.answer(message, " замуж за французского подданого Дон-Дон Штопаный Гандон. ")
 		await asyncio.sleep(4)
-		await message_id.edit(" На пир были приглашены ")
+		await utils.answer(message, " На пир были приглашены ")
 		await asyncio.sleep(4)
-		await message_id.edit(" с Румынской стороны Залупыська, ")
+		await utils.answer(message, " с Румынской стороны Залупыська, ")
 		await asyncio.sleep(4)
-		await message_id.edit(" с русской Иван-Пиздарван, ")
+		await utils.answer(message, " с русской Иван-Пиздарван, ")
 		await asyncio.sleep(4)
-		await message_id.edit(" с грузинской Целкаламидзе, ")
+		await utils.answer(message, " с грузинской Целкаламидзе, ")
 		await asyncio.sleep(4)
-		await message_id.edit(" с китайской Сунь Хуй в Чай? ")
+		await utils.answer(message, " с китайской Сунь Хуй в Чай? ")
 		await asyncio.sleep(4)
-		await message_id.edit(" Вынь Сухим и дай подержать другим. ")
+		await utils.answer(message, " Вынь Сухим и дай подержать другим. ")
 		await asyncio.sleep(4)
-		await message_id.edit(" На обед были поданы - супы горячие, хуи стаоячие. ")
+		await utils.answer(message, " На обед были поданы - супы горячие, хуи стаоячие. ")
 		await asyncio.sleep(4)
-		await message_id.edit(" После обеда был просмотр фильма ")
+		await utils.answer(message, " После обеда был просмотр фильма ")
 		await asyncio.sleep(4)
-		await message_id.edit(" -Не одна я в поле кувыркалась, не одной мне ветер в жопу дул-. ")
+		await utils.answer(message, " -Не одна я в поле кувыркалась, не одной мне ветер в жопу дул-. ")
 		await asyncio.sleep(4)
-		await message_id.edit(" После этого был показ спектакля ")
+		await utils.answer(message, " После этого был показ спектакля ")
 		await asyncio.sleep(4)
-		await message_id.edit(" -Как батька Махно выставил жопу в окно-. ")
+		await utils.answer(message, " -Как батька Махно выставил жопу в окно-. ")
 		await asyncio.sleep(4)
-		await message_id.edit(" И после этого была пропета украиньска народна письня ")
+		await utils.answer(message, " И после этого была пропета украиньска народна письня ")
 		await asyncio.sleep(4)
-		await message_id.edit(" -Ох, да залупылась на хую шкурынка- ")
+		await utils.answer(message, " -Ох, да залупылась на хую шкурынка- ")
 		await asyncio.sleep(4)
-		await message_id.edit(" Сегоднешняя газета зкончилась ")#пасхалочка
+		await utils.answer(message, " Сегоднешняя газета зкончилась ")#пасхалочка
