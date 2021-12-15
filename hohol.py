bed712d6588dcc1fecb8aafcb280afc7b99e45ec
seesonrise <89902479+seesonrise@users.noreply.github.com>
2021-12-15 19:09:22 +0200

    Add files via upload

--- hohol.py
+++ hohol.py
@@ -17,7 +17,7 @@ class HoholMod(loader.Module):
             return await message.edit("<b>Please specify who to hohol.</b>")
 
         msg = await utils.answer(message, "<b>Sending Hohol...</b>")
-        await message.client.send_vide(target, hohol_url)
+        await message.client.send_video(target, hohol_url)
         await utils.answer(msg, "<b>Sent Hohol successfully!</b>")
 
     @staticmethod
