8e0147cb01d40577902f868ed47dee56c2736cb0
seesonrise <89902479+seesonrise@users.noreply.github.com>
2021-12-15 20:01:14 +0200

    Add files via upload

---
+++ hohl.py
@@ -0,0 +1,12 @@
+from .. import loader, utils
+from asyncio import sleep
+gif_url ="https://c.tenor.com/BS0X4mOtkRIAAAAd/%D1%85%D0%BE%D1%85%D0%BE%D0%BB-happy.gif"
+@loader.tds
+class HoholMod(loader.Module):
+  async def hohlcmd(self, message):
+    for _ in range(2):
+      for sending in ['Отправка.','Отправка..','Отправка...']:
+        await utils.answer(message,sending)
+        sleep(1)
+      await utils.answer(message,"Ваш хохольчик")
+      await message.client.send_video(message,gif_url)
