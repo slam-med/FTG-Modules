244a2ffb5e3888a35475381f3f58aa9f286dcef8
seesonrise <89902479+seesonrise@users.noreply.github.com>
2021-12-17 17:21:25 +0200

    Update hohl.py

--- hohl.py
+++ hohl.py
@@ -3,7 +3,11 @@ from asyncio import sleep
 gif_url ="https://c.tenor.com/BS0X4mOtkRIAAAAd/%D1%85%D0%BE%D1%85%D0%BE%D0%BB-happy.gif"
 @loader.tds
 class HoholMod(loader.Module):
+   """Высылает Хохольчика"""
+    strings = {'name': 'Домашний хохольчик :)'}
+  @loader.owner
   async def hohlcmd(self, message):
+    """Хохольчик Радуеться"""
     for _ in range(2):
       for sending in ['Отправка.','Отправка..','Отправка...']:
         await utils.answer(message,sending)
